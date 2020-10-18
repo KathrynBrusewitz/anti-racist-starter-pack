2cfb1590e90e43fcc29f983ab3a8a426697f2955
KathrynBrusewitz <kathryn.brusewitz@gmail.com>
2020-10-17 18:55:33 -0700

    Add banner for US voter registration

---
+++ frontend/src/components/Banner.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className='banner'>
+      November 3rd is a crucial day in the United States. Register to vote at{' '}
+      <a href='https://www.vote.org/' target='_blank' rel='noopener noreferrer'>
+        Vote.org
+      </a>
+    </div>
+  );
+}
+
+export default Banner;
