535eb41f59f24cbcb1ccd073df64408c88aed86c
KathrynBrusewitz <kathryn.brusewitz@gmail.com>
2020-10-17 19:14:58 -0700

    Fix banner styles: Align banner content with page content

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -4,10 +4,12 @@ import './Banner.css';
 function Banner() {
   return (
     <div className='banner'>
-      November 3rd is a crucial day in the United States. Register to vote at{' '}
-      <a href='https://www.vote.org/' target='_blank' rel='noopener noreferrer'>
-        Vote.org
-      </a>
+      <div className={'banner-content'}>
+        November 3rd is a crucial day in the United States. Register to vote at{' '}
+        <a href='https://www.vote.org/' target='_blank' rel='noopener noreferrer'>
+          Vote.org
+        </a>
+      </div>
     </div>
   );
 }
